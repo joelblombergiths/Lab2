b599eb66af6404564fdc4a30baaf1eff91e86f98
Joel Blomberg <joel.blomberg@iths.se>
2022-10-02 01:53:14 +0200

    ...

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,33 +1,41 @@
 ﻿using GeometricShapes;
 
-List<Shape> shapes = new();
+Console.CursorVisible = false;
 
-Console.WriteLine("20 Randomly Generated Shapes:");
+int _numShapes = 20;
 
-foreach (int _ in Enumerable.Range(0,20))
+do
 {
-    Shape shape = Shape.GenerateShape();
-    shapes.Add(shape);
-    
-    Console.WriteLine(shape);
+    Console.Clear();
+    Console.WriteLine($"{_numShapes} Randomly Generated Shapes:");
+    Console.WriteLine();
+
+    List<Shape> shapes = new();
+    for (int i = 0; i < _numShapes; i++)
+    {
+        Shape shape = Shape.GenerateShape();
+        shapes.Add(shape);
+
+        Console.WriteLine(shape);
+    }
+    Console.WriteLine();
+
+    float averageArea = shapes.Average(shape => shape.Area);
+    Console.WriteLine($"Average Area of all shapes in the list is {averageArea:f2}.");
+    Console.WriteLine();
+
+    float totalTriangleCirc = shapes.Where(shape => shape is Triangle).Sum(shape => ((Triangle)shape).Circumference);
+    Console.WriteLine($"Total Circumference of all Triangles is {totalTriangleCirc:f2}.");
+    Console.WriteLine();
+
+    Shape3D shapeWithLargestVolume = (Shape3D)shapes.Where(shape => shape is Shape3D).OrderByDescending(shape => ((Shape3D)shape).Volume).First();
+    Console.WriteLine($"{shapeWithLargestVolume} has the largest volume with {shapeWithLargestVolume.Volume:f2}.");
+    Console.WriteLine();
+
+    var countShapes = shapes.GroupBy(shape => shape.GetType()).Select(group => new { Count = group.Count(), Type = group.Key }).OrderByDescending(group => group.Count).First();
+    Console.WriteLine($"Shape \"{countShapes.Type.Name}\" has most instances with {countShapes.Count} in the list.");
+    Console.WriteLine();
+
+    Console.WriteLine($"Press the Any key to randomize {_numShapes} new shapes or ESC to Exit.");        
 }
-Console.WriteLine();
-
-float averageArea = shapes.Average(s => s.Area);
-Console.WriteLine($"Average Area of all shapes in the list is {averageArea:f2}.");
-Console.WriteLine();
-
-float totalTriangleCirc = shapes.Where(s => s is Triangle).Sum(x => ((Triangle)x).Circumference);
-Console.WriteLine($"Total Circumference of all Triangles is {totalTriangleCirc:f2}.");
-Console.WriteLine();
-
-Shape3D shapeWithLargestVolume = (Shape3D)shapes.Where(s => s is Shape3D).OrderByDescending(o => ((Shape3D)o).Volume).First();
-Console.WriteLine($"{shapeWithLargestVolume} has the largest volume with {shapeWithLargestVolume.Volume:f2}.");
-Console.WriteLine();
-
-var count = shapes.GroupBy(s => s.GetType()).Select(x => new { Count = x.Count(), Type = x.Key }).OrderByDescending(o => o.Count).First();
-Console.WriteLine($"Shape \"{count.Type.Name}\" has most instances with {count.Count} in the list.");
-Console.WriteLine();
-
-Console.WriteLine("Press the Any key to Exit");
-Console.ReadKey(true);
+while (Console.ReadKey(true).Key != ConsoleKey.Escape);
--- GeometricShapes/Shape.cs
+++ GeometricShapes/Shape.cs
@@ -4,7 +4,7 @@ namespace GeometricShapes
 {
     public abstract class Shape
     {
-        private static readonly int _maxDelta = 100;
+        private static readonly int _maxDelta = 10;
         private enum Shapes
         {
             Rectangle,
@@ -19,8 +19,8 @@ namespace GeometricShapes
         public abstract Vector3 Center { get; }
         public abstract float Area { get; }
 
-        public static Shape GenerateShape() => GenerateShape(RandomVector3());
-        
+        public static Shape GenerateShape() => GenerateShape(RandomVector3(true));
+
         public static Shape GenerateShape(Vector3 position)
         {
             int randomShape = Random.Shared.Next(Enum.GetNames(typeof(Shapes)).Length);
@@ -31,26 +31,26 @@ namespace GeometricShapes
             switch (shapeType)
             {
                 case Shapes.Rectangle:
-                    shape = new Rectangle(new Vector2(position.X, position.Y), RandomVector2());
+                    shape = new Rectangle(new Vector2(position.X, position.Y), RandomVector2(false));
                     break;
                 case Shapes.Square:
-                    shape = new Rectangle(new Vector2(position.X, position.Y), RandomSize());
+                    shape = new Rectangle(new Vector2(position.X, position.Y), RandomFloat(false));
                     break;
                 case Shapes.Cuboid:
-                    shape = new Cuboid(position, RandomVector3());
+                    shape = new Cuboid(position, RandomVector3(false));
                     break;
                 case Shapes.Cube:
-                    shape = new Cuboid(position, RandomSize());
+                    shape = new Cuboid(position, RandomFloat(false));
                     break;
                 case Shapes.Circle:
-                    shape = new Circle(new Vector2(position.X, position.Y), RandomSize());
+                    shape = new Circle(new Vector2(position.X, position.Y), RandomFloat(false));
                     break;
                 case Shapes.Sphere:
-                    shape = new Sphere(position, RandomSize());
+                    shape = new Sphere(position, RandomFloat(false));
                     break;
                 case Shapes.Triangle:
-                    Vector2 p1 = RandomVector2();
-                    Vector2 p2 = RandomVector2();
+                    Vector2 p1 = RandomVector2(true);
+                    Vector2 p2 = RandomVector2(true);
                     Vector2 p3 = new(3 * position.X - p1.X - p2.X, 3 * position.Y - p1.Y - p2.Y);
                     shape = new Triangle(p1, p2, p3);
                     break;
@@ -61,19 +61,15 @@ namespace GeometricShapes
             return shape;
         }
 
-        private static float RandomSize() => (float)Random.Shared.NextDouble() * _maxDelta;
-        private static Vector2 RandomVector2() => new ((float) Random.Shared.NextDouble() * _maxDelta, (float)Random.Shared.NextDouble() * _maxDelta);        
-        private static Vector3 RandomVector3() => new ((float) Random.Shared.NextDouble() * _maxDelta, (float)Random.Shared.NextDouble() * _maxDelta, (float)Random.Shared.NextDouble() * _maxDelta);
+        private static float RandomFloat(bool signed)
+        {
+            double significand = (Random.Shared.NextDouble() * 2.0);
+            if (signed) significand -= 1.0;
+            double exponent = Math.Pow(2.0, Random.Shared.Next(_maxDelta));
+            return (float)(significand * exponent);
+        }
+
+        private static Vector2 RandomVector2(bool signed) => new(RandomFloat(signed), RandomFloat(signed));
+        private static Vector3 RandomVector3(bool signed) => new(RandomVector2(signed), RandomFloat(signed));
     }
 }
-            //Shape shape = shapeType switch
-            //{
-            //    Shapes.Rectangle => new Rectangle(new Vector2(position.X, position.Y), new Vector2(randomSize.X, randomSize.Y)),
-            //    Shapes.Square => new Rectangle(new Vector2(position.X, position.Y), randomFloat),
-            //    Shapes.Cuboid => new Cuboid(position, randomSize),
-            //    Shapes.Cube => new Cuboid(position, randomFloat),
-            //    Shapes.Circle => new Circle(new Vector2(position.X, position.Y), randomFloat),
-            //    Shapes.Sphere => new Sphere(position, randomFloat),
-            //    Shapes.Triangle => new Triangle(Vector2.One, Vector2.One * 2, Vector2.One * 4),
-            //    _ => throw new ArgumentOutOfRangeException("shapeType", "This should probably never happen :S"),
-            //};

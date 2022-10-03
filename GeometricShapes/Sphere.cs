b3c9564d02c6e7fbba985744937213ab9a348565
Joel Blomberg <joel.blomberg@iths.se>
2022-10-03 14:22:48 +0200

    ...

--- GeometricShapes/Sphere.cs
+++ GeometricShapes/Sphere.cs
@@ -4,18 +4,18 @@ namespace GeometricShapes
 {
     public class Sphere : Shape3D
     {
-        private float _radius;
+        private float radius;
         private Vector3 _center;
 
         public override Vector3 Center => _center;
-        public override float Area => 4 * MathF.PI * MathF.Pow(_radius, 2);
-        public override float Volume => 4 / 3 * MathF.PI * MathF.Pow(_radius, 3);
-        public override string ToString() => $"Sphere @({_center.X:f2}, {_center.Y:f2},  {_center.Z:f2}): r = {_radius:f2}";
+        public override float Area => 4 * MathF.PI * MathF.Pow(radius, 2);
+        public override float Volume => 4 / 3 * MathF.PI * MathF.Pow(radius, 3);
+        public override string ToString() => $"Sphere @({_center.X:f2}, {_center.Y:f2},  {_center.Z:f2}): r = {radius:f2}";
         
         public Sphere(Vector3 center, float radius)
         {
             _center = center;
-            _radius = radius;
+            this.radius = radius;
         }
     }
 }
